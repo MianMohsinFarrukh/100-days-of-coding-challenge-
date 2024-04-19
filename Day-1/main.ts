7ea9faf52ab032a56ffacd81223058f977f81294
MianMohsinFarrukh <1mohsinfarrukh@gmail.com>
2024-04-19 10:15:22 -0700

    first commit

---
+++ Day-1/main.ts
@@ -0,0 +1,29 @@
+//🚀 100 Days of Coding Challenge 🚀
+//🎯 Challenge Objective
+//🚀 Day 1 Challenge: Start Coding! 🚀
+
+
+
+//Question 1: Install Node.js, TypeScript and VS Code on your computer/Laptop.
+console.log("Installation Completed!");
+
+
+//Question 2: Personal Message: Store a person’s name in a variable and print a message to them, like “Hello Asharib, would you like to learn some TypeScript today?”
+
+
+const personName: string = " 'Mohsin' ";
+console.log(`"Hello${personName}, would you like to learn some TypeScript today?"`);
+
+
+
+//Question 3: Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+
+let lowercase: string = "MOHSIN";
+console.log(`Person Name in lowercase : ${lowercase.toLowerCase()}`);
+
+
+let uppercase :string="muhammad faraz";
+console.log(`Person Name in uppercase : ${uppercase.toUpperCase()}`);
+
+let titlecase:string="rizwan aHMED";
+console.log( "Person Name in titlecase :", titlecase.charAt(0).toUpperCase()+titlecase.slice(1).toLowerCase());
