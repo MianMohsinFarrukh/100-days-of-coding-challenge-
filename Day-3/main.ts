bd855b152840b57e01ab870783a7a2b2a82e6c43
MianMohsinFarrukh <1mohsinfarrukh@gmail.com>
2024-04-20 22:48:24 -0700

    first commit

---
+++ Day-3/main.ts
@@ -0,0 +1,29 @@
+//🚀 Day 3 Challenge: Start Coding! 🚀
+
+//Question 7: Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Enclose your operations in print statements.
+
+
+console.log(5 + 3);       // Addition
+console.log(10 - 2);     // Subtraction
+console.log(2 * 4);     // Multiplication
+console.log(16 / 2);    // Division
+
+
+
+//Question 8: You should create four lines that look like this:
+//console.log(5 + 3)
+
+console.log(5 + 3);       // Addition
+console.log(10 - 2);     // Subtraction
+console.log(2 * 4);     // Multiplication
+console.log(16 / 2);    // Division
+
+
+//Question 9: Favorite Number: Store your favorite number in a variable. Then, create a message revealing your favorite number. Print that message.
+
+const favNumber: number = 7;
+const message: string = `\n  My Favorite Number is : ${favNumber}`;
+console.log(message);
+
+
+
