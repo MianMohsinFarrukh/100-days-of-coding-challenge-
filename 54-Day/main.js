6e1505e975e93344231c7eace678714862675909
MianMohsinFarrukh <1mohsinfarrukh@gmail.com>
2024-05-21 07:24:28 -0700

    first commit

---
+++ 54-Day/main.js
@@ -0,0 +1,17 @@
+// Day-54 of 100-Days-Of-Code Challenge
+// Day-54 Task:
+
+// Read the following Articles and Code along with it:
+
+// Hello World
+
+// Variables
+
+
+// Hello World:
+// alert('Hello World'); 
+
+
+// Variables:
+let message = "Hello!"
+alert(message);
