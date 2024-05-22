14aaa57869353bbbc99cde31de55b936a18b19a6
MianMohsinFarrukh <1mohsinfarrukh@gmail.com>
2024-05-22 07:57:19 -0700

    first commit

---
+++ 55 -Day/main.js
@@ -0,0 +1,30 @@
+"use strict";
+// Day-55 of 100-Days-Of-Code Challenge
+// Day-55 Task:
+// Read the following Articles and Code along with it:
+// Data Types in JavaScript
+// Interaction: alert, prompt, confirm in JavaScript
+
+
+
+// Data Types in JavaScript :
+// string    'mohsin';
+// number      3456;
+// boolean     true/false;
+// null
+// undefined
+
+
+
+
+
+
+// Interaction: alert, prompt, confirm in JavaScript :
+
+
+alert('hello world!');
+
+// prompt('insert your age plz!');
+
+
+// confirm('hi world');
