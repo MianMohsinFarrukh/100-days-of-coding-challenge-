d00ecf6c27abe29f3c0fac1ad574f197476b6ac2
MianMohsinFarrukh <1mohsinfarrukh@gmail.com>
2024-05-24 09:37:47 -0700

    first commit

---
+++ 56-Day/main.ts
@@ -0,0 +1,11 @@
+// Day-56 of 100-Days-Of-Code Challenge
+// Day-56 Task:
+
+// Read the following Articles and Code along with it:
+
+// Type Conversions in JavaScript
+
+// Operators in JavaScript
+
+// Star GitHub Repository:
+// https://github.com/AsharibAli/100-days-of-code/tree/main/day-56
