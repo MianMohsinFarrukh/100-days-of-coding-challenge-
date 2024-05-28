dc8ba440e2432931e9e640df707e3dc380317d7f
MianMohsinFarrukh <1mohsinfarrukh@gmail.com>
2024-05-28 07:34:17 -0700

    first commit

---
+++ 57-Day/main.js
@@ -0,0 +1,9 @@
+"use strict";
+console.log('\n');
+console.log('hello world');
+console.log('\n');
+function sum(num1, num2) {
+    return num1 + num2;
+}
+;
+console.log(sum(5, 15));
--- 57-Day/main.ts
+++ 57-Day/main.ts
@@ -8,4 +8,4 @@ console.log('\n');
 function sum(num1: number, num2: number) {
     return num1 + num2
 };
-console.log(sum(5, 5));
+console.log(sum(5, 15));
