cee0a3908afccf8d486d38292cba009521c3470c
MianMohsinFarrukh <1mohsinfarrukh@gmail.com>
2024-05-29 07:23:52 -0700

    first  commit

--- 57-Day/main.js
+++ 57-Day/main.js
@@ -1,10 +1,24 @@
+// Day-57 of 100-Days-Of-Code Challenge
+// Day-57 Task:
+
+// Read the following Articles and Code along with it:
+
+// Comparisons in JavaScript
+
+// Conditional Operators: if, '?'(Ternary Operator) in JavaScript
+
+
+
+
 "use strict";
-function first() {
-    function sum(num1, num2) {
-        return num1 + num2;
-    }
-    ;
-    console.log(sum(5, 15));
+
+let accessAllowed;
+let age = prompt('How old are you?', '');
+
+if (age > 18) {
+    accessAllowed = true;
+} else {
+    accessAllowed = false;
 }
-;
-first();
+
+alert(accessAllowed);
