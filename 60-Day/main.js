0c5f0f6467437b44e18af4a7d4c29ae661c49e2f
MianMohsinFarrukh <1mohsinfarrukh@gmail.com>
2024-06-06 22:56:32 -0700

    first commit

--- 60-Day/main.js
+++ 60-Day/main.js
@@ -1,32 +1,18 @@
+"use strict";
 // Day-60 of 100-Days-Of-Code Challenge
 // Day-60 Task:
-
 // Read the following Articles and Code along with it:
-
 // Functions in JavaScript
-
 // Function Expressions in JavaScript
-
-
-
-
-
 // Functions
 // Quite often we need to perform a similar action in many places of the script.
-
 // For example, we need to show a nice-looking message when a visitor logs in, logs out and maybe somewhere else.
-
 // Functions are the main “building blocks” of the program. They allow the code to be called many times without repetition.
-
-
 // Function expressions
 // In JavaScript, a function is not a “magical language structure”, but a special kind of value.
-
 // The syntax that we used before is called a Function Declaration:
-
 // function sayHi() {
 //   alert( "Hello" );
 // }
 // There is another syntax for creating a function that is called a Function Expression.
-
 // It allows us to create a new function in the middle of any expression.
