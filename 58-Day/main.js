3794e0efa567a8975f8afaed83b677a4d925f37c
MianMohsinFarrukh <1mohsinfarrukh@gmail.com>
2024-06-06 22:47:14 -0700

    first commit

--- 58-Day/main.js
+++ 58-Day/main.js
@@ -1,19 +1,8 @@
+"use strict";
 // Day-58 of 100-Days-Of-Code Challenge
 // Day-58 Task:
-
 // Read the following Articles and Code along with it:
-
 // Logical Operators in JavaScript
-
 // Nullish Coalescing Operator '??' in JavaScript
-
-
-
-
-
-
 // Logical operators
 // There are four logical operators in JavaScript: || (OR), && (AND), ! (NOT), ?? (Nullish Coalescing). 
-
-
-
