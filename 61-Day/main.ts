59f81ac09e885f5eae8e28d55642cfc7a453face
MianMohsinFarrukh <1mohsinfarrukh@gmail.com>
2024-06-07 23:00:21 -0700

    first commit

---
+++ 61-Day/main.ts
@@ -0,0 +1,34 @@
+// Day-61 of 100-Days-Of-Code Challenge
+// Day-61 Task:
+
+// Read the following Articles and Code along with it:
+
+// Arrow functions Basics in JavaScript
+
+// JavaScript specials in JavaScript
+
+
+
+
+
+// Arrow functions, the basics
+// There’s another very simple and concise syntax for creating functions, that’s often better than Function Expressions.
+
+// It’s called “arrow functions”,
+
+
+// let sum = (a, b) => a + b;
+
+// /* This arrow function is a shorter form of:
+
+// let sum = function(a, b) {
+//   return a + b;
+// };
+// */
+
+// alert( sum(1, 2) ); // 3
+
+
+
+
+
