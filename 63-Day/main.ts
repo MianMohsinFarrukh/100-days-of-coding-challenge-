142c05aa9c8232d66966b924fc820b3cd3552810
MianMohsinFarrukh <1mohsinfarrukh@gmail.com>
2024-06-11 23:09:36 -0700

    first commit

--- 63-Day/main.ts
+++ 63-Day/main.ts
@@ -1,1 +1,9 @@
-console.log(`hello world`);
+// Day-63 of 100-Days-Of-Code Challenge
+// Day-63 Task:
+
+// Learn about TypeScript types by using the following guide and coding along with the examples provided in it:
+
+// Multiple Types in TypeScript
+
+// ✨ Star GitHub Repository 
+// https://github.com/AsharibAli/100-days-of-code
