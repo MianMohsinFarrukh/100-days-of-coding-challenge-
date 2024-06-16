669dc01163eab672f321e24ed8f2c39767931944
MianMohsinFarrukh <1mohsinfarrukh@gmail.com>
2024-06-15 23:13:55 -0700

    first commit

--- 63-Day/main.ts
+++ 63-Day/main.ts
@@ -5,10 +5,46 @@
 
 // Multiple Types in TypeScript
 
-// ✨ Star GitHub Repository 
-// https://github.com/AsharibAli/100-days-of-code
 
 
+/********************
+ * TYPE ANNOTATIONS *
+ ********************/
 
+let myNumberExOne: number = 42;
+let myStringExOne: string = "Hello, TypeScript";
+
+console.log("My Number Is:", myNumberExOne);
+console.log("My String Is:", myStringExOne);
+
+
+// Another way to write this code is as follows:
+let myNumberExTwo: number;
+let myStringExTwo: string;
+
+myNumberExTwo = 42;
+myStringExTwo = "Hello, TypeScript";
+
+console.log("My Number Is:", myNumberExTwo);
+console.log("My String Is:", myStringExTwo);
 
 console.log(`\n`);
+
+
+/******************************
+ * DYNAMIC TYPE DETERMINATION *
+ ******************************/
+
+let greeting = "Hello, world!";
+console.log(greeting);
+
+console.log(`\n`);
+
+
+/************
+ * ANY TYPE *
+ ************/
+
+let myVariable: any = 42;
+myVariable = "Hello, TypeScript";
+console.log(myVariable)
