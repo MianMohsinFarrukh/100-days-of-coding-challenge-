b1613d0d0bbf4d1bbe54acda4af1eb60d5be16d3
MianMohsinFarrukh <1mohsinfarrukh@gmail.com>
2024-06-21 23:02:10 -0700

    first commit

--- 67-Day/main.ts
+++ 67-Day/main.ts
@@ -21,3 +21,10 @@ for (let i = 0; i < fruits.length; i++) {
 
 
 
+/*************************************************
+ * TYPE ANNOTATIONS WITH MULTIDIMENSIONAL ARRAYS *
+ *************************************************/
+
+let arrayOne: number[] = [1, 2, 3, 4, 5];
+let arrayTwo: string[] = ["A", "B", "C"];
+let arrayThree: (string | number)[] = [1, 2, 3, 4, "A", "B", "C"];
