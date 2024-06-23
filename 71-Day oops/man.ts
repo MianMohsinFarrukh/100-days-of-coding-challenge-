3972e6613b88af314f1d1dffd40b6c232a37223e
MianMohsinFarrukh <1mohsinfarrukh@gmail.com>
2024-06-23 07:01:26 -0700

    first commit

---
+++ 71-Day oops/man.ts
@@ -0,0 +1,40 @@
+/************************
+ * CLASS STATIC MEMBERS *
+ ************************/
+
+
+
+
+class ProductStaticMembers {
+    // 'private' property :
+    private static nextId: number = 1;
+
+
+    constructor(private id: number, private name: string) { };
+
+    static generateNextId(): number {
+        return ProductStaticMembers.nextId++;
+    };
+
+
+    getProductInfo(): string {
+        return `ID: ${this.id}, Name: ${this.name}`;
+    };
+
+};
+
+
+// Generate unique IDs for products using the static method
+const product1static = new ProductStaticMembers(
+    ProductStaticMembers.generateNextId(),
+    "Mohsin"
+);
+
+const product2static = new ProductStaticMembers(
+    ProductStaticMembers.generateNextId(),
+    "ALi"
+);
+
+
+console.log(product1static.getProductInfo()); // ID: 1, Name: Widget
+console.log(product2static.getProductInfo()); // ID: 2, Name: Gadget
