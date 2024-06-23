bf1f9a69e442c21f3a91b4113208e3f44a81b4bb
MianMohsinFarrukh <1mohsinfarrukh@gmail.com>
2024-06-23 07:13:48 -0700

    first commit

---
+++ 71-Day oops/interface.ts
@@ -0,0 +1,39 @@
+/*****************************
+ * CLASS IMPLEMENT INTERFACE *
+ *****************************/
+
+
+
+
+class ProductImplementInterface {
+    // 'private' property
+    private static nextId: number = 1;
+
+    constructor(private id: number, private name: string) { }
+
+    static generateNextId(): number {
+        return ProductImplementInterface.nextId++;
+    }
+
+    getProductInfo(): string {
+        return `ID: ${this.id}, Name: ${this.name}`;
+    };
+
+
+};
+
+
+// Generate unique IDs for products using the static method
+const product1implement = new ProductImplementInterface(
+    ProductImplementInterface.generateNextId(),
+    "Mohsin"
+);
+const product2implement = new ProductImplementInterface(
+    ProductImplementInterface.generateNextId(),
+    "Faraz"
+);
+
+
+
+console.log(product1implement.getProductInfo()); // ID: 1, Name: Mohsin
+console.log(product2implement.getProductInfo()); // ID: 2, Name: Faraz
