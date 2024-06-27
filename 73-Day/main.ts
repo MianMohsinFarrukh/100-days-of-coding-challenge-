7a1a6b05ba4869c48421bdb47f8867f74d0efd99
MianMohsinFarrukh <1mohsinfarrukh@gmail.com>
2024-06-27 06:54:36 -0700

    first commit

---
+++ 73-Day/main.ts
@@ -0,0 +1,19 @@
+// Day-73 of 100-Days-Of-Code Challenge
+// Day-73 Task:
+
+// Learn about TypeScript type assertions by using the following guide and coding along with the examples provided in it:
+
+// Type Assertions in TypeScript :
+
+
+
+
+/*******************
+ * TYPE ASSERTIONS *
+ *******************/
+
+let data: any = "1000";
+
+
+// Type assertion to treat "data" as a string
+console.log((data as string).repeat(6));
