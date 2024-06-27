efd4e04121e18f4024e62a23d6615b1ed6186d94
MianMohsinFarrukh <1mohsinfarrukh@gmail.com>
2024-06-27 07:41:02 -0700

    first commit

---
+++ 75-Day internet/main.ts
@@ -0,0 +1,18 @@
+// Day-75 of 100-Days-Of-Code Challenge
+// Day-75 of 100-Days-Of-Code Challenge
+// Day-75 Task:
+
+// Read the following Article with interactive videos about How does the internet work? behind the scenes what happens, etc.
+
+// How does the internet work?
+
+
+
+
+
+
+
+
+
+// What is the Internet?
+// The Internet is a global network of computers connected to each other which communicate through a standardized set of protocols.
