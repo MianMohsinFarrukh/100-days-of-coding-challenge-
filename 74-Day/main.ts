b62b56e2a6e35582fee51f33c5c21e7f326db1e0
MianMohsinFarrukh <1mohsinfarrukh@gmail.com>
2024-06-27 07:24:28 -0700

    first commit

---
+++ 74-Day/main.ts
@@ -0,0 +1,47 @@
+// Day-74 of 100-Days-Of-Code Challenge
+// Day-74 Task:
+
+// Learn about TypeScript Debugging by using the following guide and debug along with it:
+
+// Debugging in TypeScript :
+
+
+
+let age: number = 22;
+if (age > 20) {                  //grater than
+    console.log('pass');
+} else if (age < 20) {           //less than
+    console.log('fail');
+} else {
+    console.log('reboot')
+};
+
+
+
+
+
+
+
+
+
+
+
+
+//*****************************************************************
+
+function newss(num1: number, num2: number) {
+    return console.log(num1 > num2); //grater than
+};
+
+newss(50, 15);
+
+
+
+
+function news(num1: number, num2: number) {
+    return console.log(num1 < num2); //less than
+};
+
+news(50, 5);
+
+
