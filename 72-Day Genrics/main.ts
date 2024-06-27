065115ec0be42b33ae25b3d9d2ff4d658eeece2c
MianMohsinFarrukh <1mohsinfarrukh@gmail.com>
2024-06-26 22:52:55 -0700

    first commit

--- 72-Day Genrics/main.ts
+++ 72-Day Genrics/main.ts
@@ -1,1 +1,37 @@
-console.log('hello world');
+// Day-72 of 100-Days-Of-Code Challenge
+// Day-72 Task:
+
+// Learn about TypeScript generics by using the following guide and coding along with the examples provided in it:
+
+// Generics in TypeScript :
+
+
+
+
+
+/******************
+ * GENERICS INTRO *
+ ******************/
+
+// A generic function that returns the input value as is
+function returnType<T>(val: T): T {
+    return val;
+  }
+  
+  // Usage of the generic function with different types
+  const numValue: number = returnType<number>(100);
+  const strValue: string = returnType<string>("Elzero");
+  const boolValue: boolean = returnType<boolean>(true);
+  const arrValue: number[] = returnType<number[]>([1, 2, 3, 4]);
+  
+  console.log(`Number Value: ${numValue}`);
+  console.log(`String Value: ${strValue}`);
+  console.log(`Boolean Value: ${boolValue}`);
+  console.log(`Array Value: ${arrValue}`);
+
+
+
+
+
+
+  
