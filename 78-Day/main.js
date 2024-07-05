7285e7472ad9e1d6ca54ccf0a5872cdc3594de4b
MianMohsinFarrukh <1mohsinfarrukh@gmail.com>
2024-07-04 23:48:45 -0700

    update

---
+++ 78-Day/main.js
@@ -0,0 +1,8 @@
+"use strict";
+console.log(`hello world`);
+console.log(`\n`);
+function sum(num1, num2) {
+    return num1 + num2;
+}
+;
+sum(5, 5);
--- 78-Day/main.ts
+++ 78-Day/main.ts
@@ -1,1 +1,10 @@
 console.log(`hello world`);
+console.log(`\n`);
+
+
+function sum(num1: number, num2: number):number {
+    return  num1 + num2 
+    
+};
+sum(5,5);
+
